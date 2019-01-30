6d569f666b8a46ae063e47ec0f1ae13d99af3f6f
Joshua Yan <jyan0602@gmail.com>
2019-01-30 00:26:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ function theBeatlesPlay(musicians, instruments){
 }
 
 function johnLennonFacts(facts){
-  var i = 1;
-  while(i >= 0){
+  var i = 0;
+  while(i < facts.length){
     facts[i] += '!!!';
   }
   return facts;
